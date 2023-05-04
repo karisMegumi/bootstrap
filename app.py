b3386fd5fd936b72b587b64b2c3e16d425d41fee
KARIS MEGUMI SHIMIZU <karis.mshimizu@senacsp.edu.br>
2023-05-04 12:02:15 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return ('aaaaaaaaaaaaaa')
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
