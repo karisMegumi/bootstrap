1a3f34ca4834edbe06f712824379ef6b113fb129
KARIS MEGUMI SHIMIZU <karis.mshimizu@senacsp.edu.br>
2023-05-09 11:53:18 -0300

    novo arquivo base

--- app.py
+++ app.py
@@ -4,11 +4,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return ('aaaaaaaaaaaaaa')
+    return render_template("index.html")
 
+@app.route('/contato')
+def contato():
+    return render_template("contato.html")
 
+@app.route('/cursos')
+def cursos():
+    return render_template("cursos.html")
 
+@app.route('/base')
+def base():
+    return render_template("base.html")
 
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/sobreEAD')
+def sobreEAD():
+    return render_template("sobreEAD.html")
 
 
 
