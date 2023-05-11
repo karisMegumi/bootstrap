6ccdd8d87bc48f5400e5be2b18ad65aa4f790899
KARIS MEGUMI SHIMIZU <karis.mshimizu@senacsp.edu.br>
2023-05-11 11:49:35 -0300

    menus

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/base')
+def base():
+    return render_template('base.html')
+
 @app.route('/contato')
 def contato():
     return render_template("contato.html")
@@ -14,17 +18,13 @@ def contato():
 def cursos():
     return render_template("cursos.html")
 
-@app.route('/base')
-def base():
-    return render_template("base.html")
-
 @app.route('/login')
 def login():
     return render_template("login.html")
 
-@app.route('/sobreEAD')
-def sobreEAD():
-    return render_template("sobreEAD.html")
+@app.route('/ead')
+def ead():
+    return render_template("ead.html")
 
 
 
