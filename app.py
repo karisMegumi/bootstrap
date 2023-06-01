9392f812e12387b5c28a35a00edbdd5843c6c44b
KARIS MEGUMI SHIMIZU <karis.mshimizu@senacsp.edu.br>
2023-06-01 12:04:37 -0300

    v2

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template, request
+import mysql.connector
+from flask import Flask, render_template, request, url_for, flash, redirect
 from forms import formLogin, formNovoUsuario 
+from hashlib import sha256
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '7272a66ced14923d1b441924bef72d1f'
 
+mydb = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'P@$$w0rd',
+    database = 'ead_senac',
+)
 
 @app.route('/')
 def index():
@@ -29,6 +37,15 @@ def login():
 
     form_login = formLogin()
     form_novo_usuario = formNovoUsuario()
+
+    if form_login.validate_on_submit() and 'submitLogin' in request.form:
+        flash(f'Login realizado com sucesso: {form_login.email.data}', 'alert-success')
+        return redirect(url_for('index'))
+    
+    if form_novo_usuario.validate_on_submit() and 'submit' in request.form:
+        cursor = mysql.cursor()
+
+        
 
     return render_template("login.html", descricao = descricao, form_login = form_login, form_novo_usuario = form_novo_usuario)
 
