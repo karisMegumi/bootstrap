c6d7ca49dc7ba725f94cc9f523dcfb807ba5f7f9
KARIS MEGUMI SHIMIZU <karis.mshimizu@senacsp.edu.br>
2023-06-20 11:41:54 -0300

    v6

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def login():
         senha = form_login.senha.data
         hashSenha = sha256(senha.encode())
 
-        comando = f'Select * from alunos where email = "{email}" '
+        comando = f"Select * from alunos where email = '{email}' "
         cursor.execute(comando)
         result = cursor.fetchall()
     
